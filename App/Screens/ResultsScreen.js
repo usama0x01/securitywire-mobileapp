043daf1b603909d66a17710494b05d54a34a4ab8
Usama Arshad <usamagujjar92@gmail.com>
2022-01-17 18:14:43 +0500

    final

--- App/Screens/ResultsScreen.js
+++ App/Screens/ResultsScreen.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect,useContext} from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import {
   StyleSheet,
   Text,
@@ -10,22 +10,20 @@ import {
   Image,
   RefreshControl,
   ActivityIndicator,
-  TouchableOpacity
-} from 'react-native';
-import { SearchBar,Card } from 'react-native-elements';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
+  TouchableOpacity,
+} from "react-native";
+import { SearchBar, Card } from "react-native-elements";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 import socketIOClient from "socket.io-client";
 import API from "../config/api";
 import apisocket from "../config/apisocket";
-import Screen from '../Components/Screen'
-import colors from '../config/colors';
-import ListItem from '../Components/ListItem';
-import {MsgBox,Colors} from '../Components/styles';
+import Screen from "../Components/Screen";
+import colors from "../config/colors";
+import ListItem from "../Components/ListItem";
+import { MsgBox, Colors } from "../Components/styles";
 
-function ResultsScreen({route,navigation}) {
-  
-
+function ResultsScreen({ route, navigation }) {
   const [isLoading, setisLoading] = useState(true);
   const [Results, setResults] = useState([]);
   const [inMemoryResults, setinMemoryResults] = useState([]);
@@ -35,90 +33,87 @@ function ResultsScreen({route,navigation}) {
   const [messageType, setmessageType] = useState("");
 
   const loadResults = async () => {
-    setmessage(null)
-    let config = {}
-      try {
-          var value = await AsyncStorage.getItem('Credentials')
-          if(value !== null) {
-              value = JSON.parse(value)
-              setUser(value)
-              config = {headers: {
-                'Authorization': 'Bearer ' + value.token
-            }}
-            }
-        } catch(e) {
-            console.log(e)
-        }
+    setmessage(null);
+    let config = {};
+    try {
+      var value = await AsyncStorage.getItem("Credentials");
+      if (value !== null) {
+        value = JSON.parse(value);
+        setUser(value);
+        config = {
+          headers: {
+            Authorization: "Bearer " + value.token,
+          },
+        };
+      }
+    } catch (e) {
+      console.log(e);
+    }
     axios
       .get(`${API}/Scanner`, config)
       .then((response) => {
         const result = response.data;
         var { status, data } = result;
-        data = JSON.stringify(data)
-        data = JSON.parse(data)
-        data = data.Scan
-          if (status != 'success') {
-            setmessage("some error occured")
-            setmessageType(status)
-            setisLoading(false)
-          }
-          else{
-            setisLoading(false)
-            data = data.filter((val)=> val.url != 'error')
-            setResults(data)
-            setinMemoryResults(data)
-          }
-        })
-        .catch((error) => {
-            setisLoading(false)
-          if(error.status == 403){
-            setmessage("Access denied, Login Again")
-          }
-          else{
-              setmessage("An error occurred. Check your network and try again")
-            }
-            console.log(error);
-            console.log("error");
-        });
+        data = JSON.stringify(data);
+        data = JSON.parse(data);
+        data = data.Scan;
+        if (status != "success") {
+          setmessage("some error occured");
+          setmessageType(status);
+          setisLoading(false);
+        } else {
+          setisLoading(false);
+          data = data.filter((val) => val.url != "error");
+          setResults(data);
+          setinMemoryResults(data);
+        }
+      })
+      .catch((error) => {
+        setisLoading(false);
+        if (error.status == 403) {
+          setmessage("Access denied, Login Again");
+        } else {
+          setmessage("An error occurred. Check your network and try again");
+        }
+        console.log(error);
+        console.log("error");
+      });
   };
 
-  useEffect(()=>{
-    setisLoading(true)
+  useEffect(() => {
+    setisLoading(true);
     loadResults();
     const socket = socketIOClient(apisocket);
-    socket.on("FromAPI", data => {
+    socket.on("FromAPI", (data) => {
       console.log("------===+===---------");
-      setisLoading(true)
+      setisLoading(true);
       loadResults();
-    
     });
-},[])
+  }, []);
 
-
-  const searchResults = value => {
-    if(value!=""){
-      const filteredResults = Results.filter(Result => {
+  const searchResults = (value) => {
+    if (value != "") {
+      const filteredResults = Results.filter((Result) => {
         return Result.url.toLowerCase().includes(value.toLowerCase());
       });
-      setsearchfield(value)
-      setinMemoryResults(filteredResults)
-    }
-    else{
-      setsearchfield(value)
-      setinMemoryResults(Results)
+      setsearchfield(value);
+      setinMemoryResults(filteredResults);
+    } else {
+      setsearchfield(value);
+      setinMemoryResults(Results);
     }
   };
-  
-    return (
-      <View style={styles.container}>
-        <Screen>
+
+  return (
+    <View style={styles.container}>
+      <Screen>
         <SearchBar
-        placeholder="Type Here..."
-        onChangeText={searchResults}
-        value={searchfield}
+          placeholder="Type Here..."
+          onChangeText={searchResults}
+          value={searchfield}
         />
-        <MsgBox type="error">{message}</MsgBox> 
-          {/* {isLoading ? (
+        <MsgBox type="error">{message}</MsgBox>
+        {/* {isLoading ? (
             <View
               style={{
                 ...StyleSheet.absoluteFill,
@@ -130,101 +125,114 @@ function ResultsScreen({route,navigation}) {
               <ActivityIndicator size="large" color="#bad555" />
             </View>
           ) : null} */}
-          <FlatList
-            data={inMemoryResults}
-            renderItem={({item}) =>
-            <View style={{marginVertical:5}}>
-            <ListItem
-            img={`https://s2.googleusercontent.com/s2/favicons?domain_url=${item.url}`}
-            title={item.url}
-            click={item.status == 'active' ? true : false}
-            status={
-              item.status == 'completed' ? (<View style={styles.statecom}>
-                <Text style={styles.t} >{item.status} </Text>
-                </View>) : item.status == 'active' ? (<View style={styles.stateact}>
-                <Text style={styles.t} >{item.status} </Text>
-                </View>) :(<View style={styles.stateerr}>
-                <Text style={styles.texts}>{item.status} </Text>
-                </View>)}
-            date={item.date.substr(0, 10)}
-            time={item.date.substr(11, 8)}
-            onPress={()=>{navigation.navigate("Report",{item:item})}}
-            />
-            </View>}
-            keyExtractor={(item, index) =>index.toString()}
-            refreshControl={
-              <RefreshControl refreshing={isLoading} onRefresh={loadResults} />
-            }
-            ListEmptyComponent={() => (
-              <View
-                style={{
-                  flex: 1,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  marginTop: 50
+        <FlatList
+          data={inMemoryResults}
+          renderItem={({ item }) => (
+            <View style={{ marginVertical: 5 }}>
+              <ListItem
+                img={`https://s2.googleusercontent.com/s2/favicons?domain_url=${item.url}`}
+                title={item.url}
+                click={item.status == "active" ? true : false}
+                status={
+                  item.status == "completed" ? (
+                    <View style={styles.statecom}>
+                      <Text style={styles.t}>{item.status} </Text>
+                    </View>
+                  ) : item.status == "active" ? (
+                    <View style={styles.stateact}>
+                      <Text style={styles.t}>{item.status} </Text>
+                    </View>
+                  ) : (
+                    <View style={styles.stateerr}>
+                      <Text style={styles.texts}>{item.status} </Text>
+                    </View>
+                  )
+                }
+                date={item.date.substr(0, 10)}
+                time={item.date.substr(11, 8)}
+                onPress={() => {
+                  navigation.navigate("Report", { item: item });
                 }}
-              >
-                {isLoading ? null : <Text style={{ color: '#000000' }}>No Results Found</Text>}
-              </View>
-            )}
-          />
-          
+              />
+            </View>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+          refreshControl={
+            <RefreshControl refreshing={isLoading} onRefresh={loadResults} />
+          }
+          ListEmptyComponent={() => (
+            <View
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+                marginTop: 50,
+              }}
+            >
+              {isLoading ? null : (
+                <Text style={{ color: "#000000" }}>No Results Found</Text>
+              )}
+            </View>
+          )}
+        />
       </Screen>
-      </View>
-    );
-  
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.light
+    backgroundColor: colors.light,
   },
-  outerContainer:{
+  outerContainer: {
     top: StatusBar.currentHeight,
   },
-  v1:{
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },v3:{
-    flexDirection: 'column',
-    justifyContent: 'space-between'
+  v1: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  box:{
-    marginVertical:5,
-    height:55,
-    backgroundColor: colors.white
+  v3: {
+    flexDirection: "column",
+    justifyContent: "space-between",
   },
-  statecom:{
-    backgroundColor: 'green',
-    borderRadius:100,
+  box: {
+    marginVertical: 5,
+    height: 55,
+    backgroundColor: colors.white,
+  },
+  statecom: {
+    backgroundColor: "green",
+    borderRadius: 100,
     width: "70%",
-    alignItems: 'center',
-    justifyContent: 'center'
-  },stateact:{
-    backgroundColor: 'yellow',
-    borderRadius:100,
-    width: "70%",
-    alignItems: 'center',
-    justifyContent: 'center'
-  },stateerr:{
-    backgroundColor: 'red',
-    borderRadius:100,
-    width: "70%",
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
-  text:{
-    fontSize:14
+  stateact: {
+    backgroundColor: "yellow",
+    borderRadius: 100,
+    width: "70%",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  texts:{
-    fontSize:14,
+  stateerr: {
+    backgroundColor: "red",
+    borderRadius: 100,
+    width: "70%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 14,
+  },
+  texts: {
+    fontSize: 14,
     color: "white",
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
-  t:{
-    fontWeight: 'bold'
-  }
+  t: {
+    fontWeight: "bold",
+  },
 });
 
 export default ResultsScreen;
--- App/Screens/ScanScreen.js
+++ App/Screens/ScanScreen.js
@@ -1,193 +1,200 @@
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Animated,
+  StyleSheet,
+  View,
+  TextInput,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { Button, ThemeProvider } from "react-native-elements";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppButton from "../Components/AppButton";
+import AppTextInput from "../Components/AppTextInput";
+import { NavigationContainer } from "@react-navigation/native";
+import urlExist from "url-exist";
 
-import React,{useState,useEffect,useRef} from 'react'
-import { Animated,StyleSheet,View,TextInput,Keyboard, TouchableWithoutFeedback } from 'react-native'
-import { Button, ThemeProvider } from 'react-native-elements';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import AppButton from '../Components/AppButton';
-import AppTextInput from '../Components/AppTextInput';
-import { NavigationContainer } from '@react-navigation/native';
-import urlExist from "url-exist"
-
-import Screen from '../Components/Screen'
+import Screen from "../Components/Screen";
 import API from "../config/api";
-import dstyles from '../config/styles';
-import { MsgBox } from '../Components/styles';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
+import dstyles from "../config/styles";
+import { MsgBox } from "../Components/styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
 
+export default ScanScreen = () => {
+  const navigation = useNavigation();
+  const [url, seturl] = useState();
+  const [message, setMessage] = useState();
+  const [messageType, setMessageType] = useState();
+  const [User, setUser] = useState();
 
-export default  ScanScreen = () => {
-  
-    const navigation = useNavigation()
-    const [url, seturl] = useState();
-    const [message, setMessage] = useState();
-    const [messageType, setMessageType] = useState();
-    const [User, setUser] = useState();
-    
-    async function validURL(str) {
-      var pattern = new RegExp('((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-      return !!pattern.test(str);
-    }
-
-    
-    useEffect(()=>{
-      async function checkUser() {
+  useEffect(() => {
+    async function checkUser() {
       try {
-          var value = await AsyncStorage.getItem('Credentials')
-          value = JSON.parse(value)
-          if(value != null)
-          {
-              setUser(value)
-          }
-      } catch(e) {
-          console.log(e)
+        var value = await AsyncStorage.getItem("Credentials");
+        value = JSON.parse(value);
+        if (value != null) {
+          setUser(value);
+        }
+      } catch (e) {
+        console.log(e);
       }
-      }
-      checkUser();
-  }
-  ,[])
+    }
+    checkUser();
+  }, []);
 
-  
-  const handleMessage = (message, type = '') => {
+  const handleMessage = (message, type = "") => {
     setMessage(message);
     setMessageType(type);
   };
-  
 
-  const sendValues =async () => {
-    setMessage(null)
-    const isLive = await urlExist(`https://${url}`)
-    if(validURL(url) && isLive ){
+  function validURL(str) {
+    var pattern = new RegExp(
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    ); // fragment locator
+    return !!pattern.test(str);
+  }
+
+  const sendValues = async () => {
+    setMessage(null);
+    const isLive = await urlExist(url);
+    console.log(isLive);
+    if (true) {
       axios
-    .get(`${API}/Scanner/status`, {
-      headers: {
-      'Authorization': 'Bearer ' + User.token
-      }
-   })
-    .then((response) => {
-      var { status, data } = response;
-      if(data.status != false){
-        setMessage("Wait for Previous Scan to complete.");
-      }else{
-        seturl(null)
-        fetch(`${API}/Scanner/create`, {
-          method: 'POST',
+        .get(`${API}/Scanner/status`, {
           headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + User.token
+            Authorization: "Bearer " + User.token,
           },
-          body: JSON.stringify({url:url })
+        })
+        .then((response) => {
+          var { status, data } = response;
+          if (data.status != false) {
+            setMessage("Wait for Previous Scan to complete.");
+          } else {
+            seturl(null);
+            fetch(`${API}/Scanner/create`, {
+              method: "POST",
+              headers: {
+                Acc6ept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + User.token,
+              },
+              body: JSON.stringify({ url: url }),
+            }).catch((error) => {
+              if (error.status == 403) {
+                setMessage("Access denied, Login Again");
+              } else {
+                setMessage(
+                  "An error occurred. Check your network and try again"
+                );
+              }
+              console.log(error);
+              console.log("error-inner");
+            });
+            navigation.navigate("Results");
+          }
         })
         .catch((error) => {
-          if(error.status == 403){
-            setMessage("Access denied, Login Again")
-          }
-          else{
-              setMessage("An error occurred. Check your network and try again")
-            }
-            console.log(error);
-            console.log("error-inner");
-        });
-        navigation.navigate("Results");
-      }
-      })
-      .catch((error) => {
-        if(error.status == 403){
-          setMessage("Access denied, Login Again")
-        }
-        else{
-            setMessage("An error occurred. Check your network and try again")
+          if (error.status == 403) {
+            setMessage("Access denied, Login Again");
+          } else {
+            setMessage("An error occurred. Check your network and try again");
           }
           console.log(error);
           console.log("error");
-      });
+        });
+    } else {
+      setMessage("Enter Valid Url.");
     }
-    else{
-      setMessage("Enter Valid Url.")
-    }
-
   };
 
-    const widthAnim = useRef(new Animated.Value(250)).current;
+  const widthAnim = useRef(new Animated.Value(250)).current;
 
-    const widthinc = () => {
-        Animated.timing(widthAnim, {
-          toValue: 350,
-          duration: 500,
-          useNativeDriver: false
-        }).start();
-      };
-    
-      const widthdec = () => {
-        Animated.timing(widthAnim, {
-          toValue: 250,
-          duration: 150,
-          useNativeDriver: false
-        }).start();
-      };
+  const widthinc = () => {
+    Animated.timing(widthAnim, {
+      toValue: 350,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const widthdec = () => {
+    Animated.timing(widthAnim, {
+      toValue: 250,
+      duration: 150,
+      useNativeDriver: false,
+    }).start();
+  };
 
   return (
-      <Screen>
-          <TouchableWithoutFeedback onPress={() => {
-              Keyboard.dismiss()
-              widthdec()
-              }}>
-       <View style={{flex:1,
-        justifyContent:'center',
-        alignContent:'center',
-        backgroundColor: dstyles.colors.light}}>
-        <Animated.View style={{width:widthAnim,alignSelf:'center'}}>
-                <View style={styles.container}>
-                    {/* <MaterialCommunityIcons name="search-web" size={30} color={dstyles.colors.medium} />   */}
-                    <TextInput onFocus={()=>{widthinc()}} value={url} onChangeText={text => seturl(text)} placeholder="Enter Url (www.redacted.com)" style={[dstyles.text,styles.inp]} color="black" autoCapitalize="none" autoCorrect={false}/>
-                </View>
-            </Animated.View>
+    <Screen>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+          widthdec();
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignContent: "center",
+            backgroundColor: dstyles.colors.light,
+          }}
+        >
+          <Animated.View style={{ width: widthAnim, alignSelf: "center" }}>
+            <View style={styles.container}>
+              {/* <MaterialCommunityIcons name="search-web" size={30} color={dstyles.colors.medium} />   */}
+              <TextInput
+                onFocus={() => {
+                  widthinc();
+                }}
+                value={url}
+                onChangeText={(text) => seturl(text)}
+                placeholder="Enter Url (www.redacted.com)"
+                style={[dstyles.text, styles.inp]}
+                color="black"
+                autoCapitalize="none"
+                autoCorrect={false}
+              />
+            </View>
+          </Animated.View>
 
-          <View style={styles.button}>  
-           <AppButton onPress={sendValues} title="Scan"  color="danger"/>
+          <View style={styles.button}>
+            <AppButton onPress={sendValues} title="Scan" color="danger" />
           </View>
-          <MsgBox type={messageType}>{message}</MsgBox>  
-       </View>
-          </TouchableWithoutFeedback>
-      </Screen>
-   
+          <MsgBox type={messageType}>{message}</MsgBox>
+        </View>
+      </TouchableWithoutFeedback>
+    </Screen>
   );
 };
 
-
-
-
-
-
-
-
 const styles = StyleSheet.create({
-    container:{
-        flexDirection: 'row',
-        backgroundColor: dstyles.colors.white,
-        borderRadius:20,
-      padding:11,
-        overflow:'hidden',
-        height:50
-
-    },
-    logo:{
-        padding:10,
-    },
-    inp:{
-        width:"100%",
-    },button: {
-      width: "40%",
-      padding: 5,
-      borderRadius: 10,
-      alignSelf:'center'
-    }
-  });
-  
-
+  container: {
+    flexDirection: "row",
+    backgroundColor: dstyles.colors.white,
+    borderRadius: 20,
+    padding: 11,
+    overflow: "hidden",
+    height: 50,
+  },
+  logo: {
+    padding: 10,
+  },
+  inp: {
+    width: "100%",
+  },
+  button: {
+    width: "40%",
+    padding: 5,
+    borderRadius: 10,
+    alignSelf: "center",
+  },
+});
